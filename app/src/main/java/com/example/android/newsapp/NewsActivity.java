44e2cbf844e1cd2c963f1c130a3629b24cdad7d3
Tam Love <dark_angel4eva@email.com>
2017-07-10 17:56:39 +0100

    Added swipe to refresh.

--- app/src/main/java/com/example/android/newsapp/NewsActivity.java
+++ app/src/main/java/com/example/android/newsapp/NewsActivity.java
@@ -9,6 +9,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.preference.PreferenceManager;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,7 +25,7 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class NewsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Article>>{
+public class NewsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Article>> {
 
     public static final String LOG_TAG = NewsActivity.class.getName();
     // URL for article data from the Guardian News API
@@ -42,6 +43,7 @@ public class NewsActivity extends AppCompatActivity implements LoaderManager.Loa
     ConnectivityManager cm;
     NetworkInfo activeNetwork;
     boolean isOnline;
+    private SwipeRefreshLayout mSwipeRefresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -89,7 +91,7 @@ public class NewsActivity extends AppCompatActivity implements LoaderManager.Loa
         // Get a reference to the LoaderManager, in order to interact with loaders.
         LoaderManager loaderManager = getLoaderManager();
 
-        cm = (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        cm = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
 
         activeNetwork = cm.getActiveNetworkInfo();
 
@@ -107,12 +109,32 @@ public class NewsActivity extends AppCompatActivity implements LoaderManager.Loa
             mEmptyTextView.setText(R.string.no_internet);
         }
 
+        // Reload the API if the user refreshes the activity
+        mSwipeRefresh = (SwipeRefreshLayout) findViewById(R.id.swiperefresh);
+        mSwipeRefresh.setOnRefreshListener(
+                new SwipeRefreshLayout.OnRefreshListener() {
+                    @Override
+                    public void onRefresh() {
+                        Log.i(LOG_TAG, "onRefresh called from SwipeRefreshLayout");
+                        if (isOnline) {
+                            refreshView();
+                        }
+                    }
+                }
+        );
+    }
+
+    public void refreshView(){
+        // Restart the loader. Pass in the int ID constant defined above and pass in null for
+        // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+        // because this activity implements the LoaderCallbacks interface).
+        getLoaderManager().restartLoader(ARTICLE_LOADER_ID, null, NewsActivity.this);
+        Log.i(LOG_TAG, "Loader on refresh");
     }
 
     @Override
     public Loader<List<Article>> onCreateLoader(int id, Bundle args) {
         // Create a new loader for the given URL
-        String fullQueryURL = "https://content.guardianapis.com/search?q=artanddesign&format=json&show-fields=all&page-size=20&api-key=test";
         Log.i(LOG_TAG, "Loader on create");
 
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
@@ -135,13 +157,14 @@ public class NewsActivity extends AppCompatActivity implements LoaderManager.Loa
         uriBuilder.appendQueryParameter("api-key", API_TEST_KEY);
         Log.v(LOG_TAG, uriBuilder.toString());
 
-
         return new ArticleLoader(this, uriBuilder.toString());
     }
 
     @Override
     public void onLoadFinished(Loader<List<Article>> loader, List<Article> articles) {
 
+        // Finish refreshing
+        mSwipeRefresh.setRefreshing(false);
         // Set empty state text to display "No articles found."
         mProgressView.setVisibility(View.GONE);
         mEmptyTextView.setText(R.string.no_articles);
@@ -171,12 +194,21 @@ public class NewsActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            Intent settingsIntent = new Intent(this, SettingsActivity.class);
-            startActivity(settingsIntent);
-            return true;
+        switch (item.getItemId()) {
+
+            // Check if user triggered a refresh:
+            case R.id.menu_refresh:
+                Log.i(LOG_TAG, "Refresh menu item selected");
+                // Signal SwipeRefreshLayout to start the progress indicator
+                mSwipeRefresh.setRefreshing(true);
+                refreshView();
+                return true;
+            case R.id.action_settings:
+                Intent settingsIntent = new Intent(this, SettingsActivity.class);
+                startActivity(settingsIntent);
+                return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 }
